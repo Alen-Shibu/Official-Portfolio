07644489c46399c6b3fa820650537fb5ddfe5b39
Alen Shibu <your-email@example.com>
2025-09-23 07:26:24 +0800

    Fixed a bug

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 const tablinks = document.querySelectorAll(".tab-links");
 const tabcontents = document.querySelectorAll(".tab-contents");
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.target.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.target.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
 // -----Services Modal Js-----
@@ -49,33 +49,43 @@ document.querySelectorAll(".modal-container").forEach((modal) => {
 const menu_icon = document.getElementById("menu-icon");
 const navbar = document.getElementById("nav");
 
-menu_icon.onclick = ()=>{
+menu_icon.onclick = () => {
   navbar.classList.toggle("active");
   menu_icon.classList.toggle("fa-xmark");
-}
+};
 
-document.addEventListener('click',(e)=>{
-  if((!navbar.contains(e.target))&&(!menu_icon.contains(e.target))){
-  navbar.classList.remove("active");
-  menu_icon.classList.remove("fa-xmark");
-  }})
+document.addEventListener("click", (e) => {
+  if (!navbar.contains(e.target) && !menu_icon.contains(e.target)) {
+    navbar.classList.remove("active");
+    menu_icon.classList.remove("fa-xmark");
+  }
+});
 
-  // Navbar hiding when scrolled down
+// Navbar hiding when scrolled down
 
-  let scrollCount = 0;
-  const header = document.querySelector("header");
-  window.addEventListener("scroll",()=>{
-    const currentScroll = window.pageYOffset;
-    if(currentScroll<=0){
-      header.classList.remove("hide");
-      scrollCount = 0;
-      return
-    }
-    if(currentScroll>scrollCount){
-      header.classList.add("hide");
-    }
-    else{
-      header.classList.remove("hide");
-    }
-    scrollCount = currentScroll;
-  })
+let scrollCount = 0;
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    header.classList.remove("hide");
+    scrollCount = 0;
+    return;
+  }
+  if (currentScroll > scrollCount) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+  scrollCount = currentScroll;
+});
+
+// Form resetting after submission
+
+const form = document.querySelector("form");
+form.addEventListener("submit", () => {
+  form.reset();
+  if (typeof grecaptcha !== "undefined") {
+    grecaptcha.reset();
+  }
+});
